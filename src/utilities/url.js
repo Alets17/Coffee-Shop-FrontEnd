d4b871ccdd7bf37169e1763edd48ca4564d91e3b
Alejandro <jarocho.extremo@hotmail.com>
2024-04-10 23:29:23 -0700

    Second

--- src/utilities/url.js
+++ src/utilities/url.js
@@ -1,2 +1,2 @@
-export const URL = 'http://localhost:5050';
+export const URL = 'https://coffee-shop-backend-production-02c4.up.railway.app';
 // export const URL = 'https://csis3380expressserver.uw.r.appspot.com';
